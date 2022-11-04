d8520504d2c0f68a54ac1dc812adc97e80bb6a56
abiwinanda <abiwinanda@outlook.com>
2022-11-04 10:05:23 +0530

    add scrape_pages/1 in PageProducer

--- lib/scraper/page_producer.ex
+++ lib/scraper/page_producer.ex
@@ -7,11 +7,25 @@ defmodule PageProducer do
     GenStage.start_link(__MODULE__, intial_state, name: __MODULE__)
   end
 
+  @doc """
+  Can be invoked to dispact event to consumer when needed.
+  """
+  def scrape_pages(pages) when is_list(pages) do
+    GenStage.cast(__MODULE__, {:pages, pages})
+  end
+
   def init(initial_state) do
     Logger.info("PageProducer init")
     {:producer, initial_state}
   end
 
+  def handle_cast({:pages, pages}, state) do
+    {:noreply, pages, state}
+  end
+
+  @doc """
+  Only invoked when consumer is requesting for a demand.
+  """
   def handle_demand(demand, state) do
     Logger.info("PageProducer received demand for #{demand} pages")
     events = []
